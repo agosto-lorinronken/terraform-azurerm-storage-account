f7bff3cec05d596a6ef0de36d45d3a176ffa6bbc
lo_ronk <ronken@pythian.com>
2021-06-04 10:27:29 -0500

    remoing dependency for provider configuration

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.56.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-  }
-}
-
-provider azurerm {
-  features {}
-}
-
 locals {
   default_event_rule = {
     event_delivery_schema = null
